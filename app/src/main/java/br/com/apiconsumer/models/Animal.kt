9da7beef7151afb2e97d8aa11357f1e891b8584c
Pedro Nascimento <pedromarsn@gmail.com>
2022-10-13 23:05:47 -0300

    main screen and animal class added

---
+++ app/src/main/java/br/com/apiconsumer/models/Animal.kt
@@ -0,0 +1,20 @@
+package br.com.apiconsumer.models
+
+class Animal {
+
+     var  name  = ""
+     var  latin_name = ""
+     var animal_type = ""
+     var active_time =  ""
+     var length_min = ""
+     var length_max = ""
+     var weight_min = ""
+     var weight_max = ""
+     var lifespan =  ""
+     var habitat =  ""
+     var diet = ""
+     var geo_range = ""
+     var image_link = ""
+     var id = 0
+
+}
